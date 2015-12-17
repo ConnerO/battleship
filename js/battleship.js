b9587eb514fbf8d3dd3194b853c0567544e723dd
Conner O'Heeron <conneroheeron@gmail.com>
2015-12-17 13:53:02 -0600

    set up the view and now Im creating the model

--- js/battleship.js
+++ js/battleship.js
@@ -1,31 +1,57 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while ( isSunk == false ) {
-	guess = prompt("What is your guess? (enter a number between 0-6):");
+// while ( isSunk == false ) {
+// 	guess = prompt("What is your guess? (enter a number between 0-6):");
 
-	if ( guess < 0 || guess > 6) {
-		alert("Please enter a valid number!");
-	} else {
-		guesses = guesses + 1;
-		if (guess == location1 || guess == location2 || guess == location3)	{	
-			hits = hits + 1;
-			alert("HIT!");
-			if (hits === 3) {
-				isSunk = true;
-				alert("You sank my battleship!");
-			}
-		}	else {
-			alert("Miss!");
-		}
+// 	if ( guess < 0 || guess > 6) {
+// 		alert("Please enter a valid number!");
+// 	} else {
+// 		guesses = guesses + 1;
+// 		if (guess == location1 || guess == location2 || guess == location3)	{	
+// 			hits = hits + 1;
+// 			alert("HIT!");
+// 			if (hits === 3) {
+// 				isSunk = true;
+// 				alert("You sank my battleship!");
+// 			}
+// 		}	else {
+// 			alert("Miss!");
+// 		}
+// 	}
+// }
+// var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + ((3/guesses)*100);
+// alert(stats);
+
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+
+	displayMiss: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
 	}
-}
-var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
-alert(stats);
 
+}; 
 
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("blah blah blah");
