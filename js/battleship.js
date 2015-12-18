a360ae16d0e5d21c0ce53b8590a4545cb7c8c7b6
Conner O'Heeron <conneroheeron@gmail.com>
2015-12-17 18:32:54 -0600

    the model has been added

--- js/battleship.js
+++ js/battleship.js
@@ -47,11 +47,54 @@ var view = {
 
 }; 
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+	
+	ships = 
+		[{ locations: ["06", "16", "26"], hits: ["", "", ""] }, 
+		 { locations: ["24", "34", "44"], hits: ["", "", ""] }, 
+		 { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+
+	fire: function(guess) {
+
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			var index = ship.locations.indexOf(guess);
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!");
+				if (this.isSunk(ship)) {
+					this.shipsSunk++;
+				}
+				return true;
+			}
+		}
+		view.displayMiss(guess);
+		view.displayMessage("You missed.");
+		return false;
+	},
+
+	isSunk: function(ship) {
+		for (var i = 0; i < this.numShips; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+
+
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
 
 view.displayMessage("blah blah blah");
